717b44f8d6a9f7f010c670dd5d3ce5a242920246
Exote <moe@threewisemeh.com>
2011-10-10 14:47:51 +0100

    removing web files, putting them in separate repo

--- WebInterface/footer.php
+++
@@ -1,4 +0,0 @@
-<div id="footer" class="clear" style="text-align:center;">
-	Made by Exote. WebAuction v0.3<br/>
-    For more usage info visit the <a href="http://dev.bukkit.org/server-mods/webauction">BukkitDev site</a>.
-</div>
